3aa8596d6390d252cb7d5b091bc763cffc9a9a47
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-11 16:27:39 +0300

    AnimalInteraction interface

---
+++ src/main/java/org/develhope/oop/animalintereaction/AnimalInteraction.java
@@ -0,0 +1,13 @@
+package org.develhope.oop.animalintereaction;
+/*
+a. Create an Interface AnimalInteraction with the following methods:
+
+void feed(String food);
+void play();
+void showInteractionHistory();
+ */
+public interface AnimalInteraction {
+    void feed(String food);
+    void play();
+    void showInteractionHistory();
+}
